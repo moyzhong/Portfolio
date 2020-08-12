b4d9da725b63eb1aa6902a86324d0d59953681df
Moy Zhong <moo@moys-mbp.lan>
2020-08-11 23:24:22 -0500

    End of lesson 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,30 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
+/*
   <Layout>
     <SEO title="Home" />
-    <h1>“Learn to <br />design and code React apps”</h1>
-    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. </p>
+  
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
+
+  /*
+  ESTABLISHING HERO ELEMENT STYLE
+  Groups act similar to groups in Figma
+  */ 
+ <Layout>
+   <div className = "Hero">
+     <div className = "HeroGroup">
+      <h1>“Learn to <br />design and code React apps”</h1>
+      <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. </p>
+      <Link to="/page-2">Watch the video</Link>
+     </div>
+   </div>
+ </Layout>
 )
 
 export default IndexPage

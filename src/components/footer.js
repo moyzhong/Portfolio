3c0598e0e3a63543b339e9fd6b6b4c0350c05f68
Moy Zhong <moo@moys-mbp.lan>
2020-08-18 15:22:34 -0500

    got rid of data.

--- src/components/footer.js
+++ src/components/footer.js
@@ -62,7 +62,7 @@ const Footer = ({data, children}) => (
 <FooterGroup>
      <Text>Tweet "Prototype and build apps with React and Swift. New courses by @MengTo."</Text>
      <Button>Tweet</Button>
-     <LinkGroup>{data.allContentfulLink.edges.map(edge => 
+     <LinkGroup>{allContentfulLink.edges.map(edge => 
         (<a href={edge.node.url}>{edge.node.title}</a>
         ))}
      </LinkGroup>
